b2e48764d7c54116cd712f9418cf427f0e6a089e
Ze Pedro Resende <17102689+ZePedroResende@users.noreply.github.com>
2021-12-12 03:55:31 +0000

    Day 11

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,63 +1,88 @@
 use crate::{load_file, parse_lines, AoCDay};
 pub struct Code;
 
+fn calculate(matrix: &mut Vec<Vec<usize>>, c: usize, r: usize) -> usize {
+    if matrix[r][c] < 10 || matrix[r][c] == 0 {
+        return 0;
+    }
+    let mut amount = 1;
+    matrix[r][c] = 0;
 
-fn neighbors(
-    y: usize,
-    x: usize,
-    width: usize,
-    height: usize,
-) -> impl Iterator<Item = (usize, usize)> {
-    println!("{} {} {} {}", y, x, width, height);
-    let mut vector = vec![(y < height - 1, (y + 1, x)), (x < width - 1, (y, x + 1))];
-    if y != 0 {
-        vector.push((y > 0, (y - 1, x)));
+    let mut neighbours = vec![(r, c + 1), (r + 1, c), (r + 1, c + 1)];
+
+    if r > 0 {
+        neighbours.push((r - 1, c));
+        neighbours.push((r - 1, c + 1));
     }
 
-    if x != 0 {
-        vector.push((x > 0, (y, x - 1)));
+    if c > 0 {
+        neighbours.push((r, c - 1));
+        neighbours.push((r + 1, c - 1));
     }
 
-    vector
-        .into_iter()
-        .filter_map(|(cond, value)| if cond { Some(value) } else { None })
-}
+    if r > 0 && c > 0 {
+        neighbours.push((r - 1, c - 1));
+    }
 
-fn tick(matrix: &mut Vec<Vec<u32>>) {
-
-    for row in matrix {
-        for item in row {
-            item +=1;
+    for &(r, c) in &neighbours {
+        //if r >= matrix.len() && c >= matrix[0].len()
+        if matrix.get(r).and_then(|row| row.get(c)).is_none() {
+            continue;
+        }
+        if matrix[r][c] != 0 {
+            matrix[r][c] += 1;
+            amount += calculate(matrix, c, r);
         }
     }
 
-    for row in matrix {
-        for item in row {
-            item +=1;
+    amount
+}
+
+fn tick(matrix: &mut Vec<Vec<usize>>) -> usize {
+    let mut amount = 0;
+
+    for line in 0..10 {
+        for row in 0..10 {
+            matrix[line][row] += 1;
         }
     }
 
+    for i in 0..10 {
+        for j in 0..10 {
+            amount += calculate(matrix, i, j);
+        }
+    }
 
+    amount
 }
-
-
 
 impl AoCDay for Code {
     fn part1(&self, _input: &mut dyn std::io::Read, _extra_argss: &[String]) -> String {
         let data = load_file(_input);
-        let lines: Vec<Vec<u32>> = parse_lines::<String>(&data)
-            .map(|str| str.chars().map(|char| char.to_digit(10).unwrap()).collect())
+        let mut lines: Vec<Vec<usize>> = parse_lines::<String>(&data)
+            .map(|str| {
+                str.chars()
+                    .map(|char| char.to_digit(10).unwrap() as usize)
+                    .collect()
+            })
             .collect();
 
+        let sum: usize = (0..100).map(|_| tick(&mut lines)).sum::<usize>();
 
-        lines
-
-
-        let number = 10;
-
-        number.to_string()
+        sum.to_string()
     }
     fn part2(&self, _input: &mut dyn std::io::Read, _extra_args: &[String]) -> String {
-        todo!()
+        let data = load_file(_input);
+        let mut lines: Vec<Vec<usize>> = parse_lines::<String>(&data)
+            .map(|str| {
+                str.chars()
+                    .map(|char| char.to_digit(10).unwrap() as usize)
+                    .collect()
+            })
+            .collect();
+
+        let sum: usize = (0..).find(|_| tick(&mut lines) == 100).unwrap() + 1;
+
+        sum.to_string()
     }
 }
