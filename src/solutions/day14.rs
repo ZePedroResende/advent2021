69b622386e5c66605fba7be0fde2f0faf95e040b
ZePedroResende <j.p.resende68@gmail.com>
2021-12-15 03:59:39 +0000

    Day 14 spacing and cleanup

--- src/solutions/day14.rs
+++ src/solutions/day14.rs
@@ -8,10 +8,12 @@ fn calculate(rules_str: &str, template: &str, iteration: usize) -> u64 {
         |mut acc: HashMap<(char, char), char>, l: &str| {
             let (element_a, element_b) = l.rsplit_once(" -> ").unwrap();
             let mut chars = element_a.chars();
+
             acc.insert(
                 (chars.next().unwrap(), chars.next().unwrap()),
                 element_b.chars().next().unwrap(),
             );
+
             acc
         },
     );
@@ -36,9 +38,11 @@ fn calculate(rules_str: &str, template: &str, iteration: usize) -> u64 {
     for _ in 0..iteration {
         for (pair, times) in counter_pairs.clone().iter() {
             let value: &char = rules.get(pair).unwrap();
+
             *counter_pairs.entry((pair.0, *value)).or_insert(0) += *times as u64;
             *counter_pairs.entry((*value, pair.1)).or_insert(0) += *times as u64;
             *counter_pairs.entry(*pair).or_insert(0) -= *times as u64;
+
             *counter_chars.entry(*value).or_insert(0) += *times as u64;
         }
     }
@@ -54,6 +58,7 @@ impl AoCDay for Code {
     fn part1(&self, _input: &mut dyn std::io::Read, _extra_argss: &[String]) -> String {
         let data = load_file(_input);
         let (template, rules_str) = data.split_once("\n\n").unwrap();
+
         calculate(&rules_str, &template, 10).to_string()
     }
 
